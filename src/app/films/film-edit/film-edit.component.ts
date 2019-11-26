7914c811d47426d6e82dadeba2013c3fbdf18355
leelaplap <letandat27101998@gmail.com>
2019-11-26 08:47:15 +0700

    construct index

--- src/app/films/film-edit/film-edit.component.ts
+++ src/app/films/film-edit/film-edit.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {FilmService} from '../../services/film.service';
 
 @Component({
   selector: 'app-film-edit',
@@ -6,8 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./film-edit.component.scss']
 })
 export class FilmEditComponent implements OnInit {
+  updateFilmForm: FormGroup;
+  index = +this.activeRoute.snapshot.paramMap.get('id');
 
-  constructor() { }
+
+  constructor(private activeRoute: ActivatedRoute,
+              private filmService: FilmService,
+              private fb: FormBuilder,
+              private router: Router
+  ) {
+  }
 
   ngOnInit() {
   }
